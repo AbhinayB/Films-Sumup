d271709606d08d86bee8f8e55c095ba2e527224a
Abhinay Bali <abhinay.bali@gmail.com>
2018-03-17 19:22:36 -0400

    Add files via upload

--- apiConnection2.js
+++ apiConnection2.js
@@ -1,4 +1,22 @@
 var indent_k;
+var predrop='url(backdrop.png)';
+var movielist = [];
+
+
+$(function(){
+   if(!localStorage.getItem("listofpre_movies"))
+   {
+      localStorage.setItem("listofpre_movies", JSON.stringify(movielist));
+   }
+   else
+   {
+       var retrievedData = localStorage.getItem("listofpre_movies");
+       movielist = JSON.parse(retrievedData);
+       for (var i = 0; i < movielist.length; i++) {
+            previous_data(movielist[i]);
+      }
+   }
+});
 $.ajax({
   url: "library_js509.json",
   dataType: 'json',
@@ -51,7 +69,61 @@ fetch(url)
         });
 }
 
-
+function previous_data(id_img)
+{
+    var poster="https://api.themoviedb.org/3/movie/"+id_img+"?api_key="+indent_k;
+    var lst=document.getElementById('movid');
+    fetch(poster)
+        .then((resp) => resp.json())
+        .then(function(data) {
+      let dv  = createNode('div');
+          dv.style.width='140px';
+          dv.style.display='inline-block';
+          //title":"Industrial Symphony No. 1: The Dream of the Brokenhearted","video":false,"vote_average":5.7,"vote_count":9
+      let spm = createNode('spam');
+          spm.innerHTML="<p>Title:"+data.title+"</p>Rating:"+data.vote_average+"</p><p>Ratings:"+data.vote_count+"</p>";
+      let cls=createNode('img');
+          cls.style.width='25px';
+          cls.style.height='25px';
+          cls.id=data.runtime;
+          cls.src="closebutton.png";
+          cls.alt=id_img;
+          spm.appendChild(cls);
+          dv.appendChild(spm);
+      let img = createNode('img');
+          img.style.width='135px';
+          img.style.height='205px';
+          dv.appendChild(img);
+          if(data.poster_path!=null)
+          {
+          img.src="https://image.tmdb.org/t/p/w185/"+data.poster_path; 
+          img.addEventListener('load', function() { 
+              $(dv).hide().appendTo(lst).fadeIn(4000);
+          }, false);
+      }else{
+          img.src="movie.png";
+          img.addEventListener('load', function() { 
+              $(dv).hide().appendTo(lst).fadeIn(4000);
+          }, false);
+      }
+      if(data.backdrop_path!=null)
+          {
+          var bpimg="url(https://image.tmdb.org/t/p/w1280//"+data.backdrop_path+")";
+          predrop=bpimg;
+          $('.glass2').css('background-image',bpimg);
+      }else{
+          $('.glass2').css('background-image',predrop);
+      }
+      if(data.runtime!=null)
+      {
+      tot_time=tot_time+parseInt(data.runtime);
+      spin_time(tot_time);
+  }
+    })
+        .catch(function(error) {
+        alert(error);
+        });
+}
 function adder(id_img)
 {
     var poster="https://api.themoviedb.org/3/movie/"+id_img+"?api_key="+indent_k;
@@ -69,6 +141,7 @@ function adder(id_img)
           cls.style.width='25px';
           cls.style.height='25px';
           cls.id=data.runtime;
+          cls.alt=id_img;
           cls.src="closebutton.png";
           spm.appendChild(cls);
           dv.appendChild(spm);
@@ -91,14 +164,20 @@ function adder(id_img)
       if(data.backdrop_path!=null)
           {
           var bpimg="url(https://image.tmdb.org/t/p/w1280//"+data.backdrop_path+")";
+          predrop=bpimg;
           $('.glass2').css('background-image',bpimg);
       }else{
-          $('.glass2').css('background-image','url(backdrop.png)');
+          $('.glass2').css('background-image',predrop);
       }
       if(data.runtime!=null)
       {
       tot_time=tot_time+parseInt(data.runtime);
       spin_time(tot_time);
+  }
+  if(movielist.indexOf(id_img)==-1)
+  {
+      movielist.push(id_img);
+      localStorage.setItem("listofpre_movies", JSON.stringify(movielist));
   }
     })
         .catch(function(error) {
@@ -111,10 +190,16 @@ $("#movid").on("click", "spam img", function (event) {
     $(this).parent('spam').parent('div').fadeOut(700,function(){
         $(this).parent('spam').parent('div').remove();
     });
-    if(this.id!='null')
+    var index_curr=movielist.indexOf(this.alt);
+    if(index_curr!=-1)
+    {
+    movielist.splice(index_curr, 1);
+    localStorage.setItem("listofpre_movies", JSON.stringify(movielist));
+    if(this.id!=null)
     {
     tot_time=tot_time-parseInt(this.id);
     spin_time(tot_time);
+    }
     }
 });
 
